bf765deaa4ff10753b0ca59623a76f74625695d8
Carlos Martin <carlos.martin@columbia.edu>
2016-04-08 16:31:23 -0400

    Added example of magnetic field induced by current loop

--- simulation.js
+++ simulation.js
@@ -139,11 +139,31 @@ var time = 0;
 var loop = function () {
     update();
     render();
+
+    // Oscillator example
+    /*
     var amplitude = 100000 * (1-Math.exp(-time));
     var frequency = 20;
     current_y[width/2][height/2-1] = amplitude * Math.sin(time * frequency);
     current_y[width/2][height/2] = amplitude * Math.sin(time * frequency);
+    */
+
+    // Magnetic field induced by current loop
+    var current = 100 * (1-Math.exp(-time));
+    var left = 100;
+    var top = 100;
+    var right = 400;
+    var bottom = 200;
+    for (var i = left; i < right; ++i) {
+        current_x[i][top] = current;
+        current_x[i][bottom] = -current;
+    }
+    for (var j = top; j < bottom; ++j) {
+        current_y[left][j] = -current;
+        current_y[right][j] = current;
+    }
     time += timestep;
+    console.log(electric_x[100][100]);
 };
 
 start();
